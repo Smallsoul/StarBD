7bbb94ea88f2aa4cb39bc4f46db9781c01121d60
Smallsoul <36156971+Smallsoul@users.noreply.github.com>
2019-09-11 13:39:12 +0400

    context update

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -23,11 +23,21 @@ import './app.css';
 
 export default class App extends Component {
 
-  swapiService = new SwapiService();
-
   state = {
-    showRandomPlanet: true
+    showRandomPlanet: true,
+    swapiService: new DummySwapiService()
   };
+
+  onServiceChange = () => {
+    this.setState(({ swapiService }) => {
+      const Service = swapiService instanceof SwapiService ?
+                            DummySwapiService : SwapiService
+
+      return {
+        swapiService: new Service()
+      }
+    })
+  }
 
   toggleRandomPlanet = () => {
     this.setState((state) => {
@@ -46,9 +56,8 @@ export default class App extends Component {
     const { getPerson,
             getStarship,
             getPersonImage,
-            getStarshipImage,
-            getAllPeople,
-            getAllPlanets } = this.swapiService;
+            getStarshipImage
+          } = this.state.swapiService;
 
     const personDetails = (
       <ItemDetails
@@ -76,9 +85,9 @@ export default class App extends Component {
 
     return (
       <ErrorBoundry>
-        <SwapiServiceProvider value={this.swapiService} >
+        <SwapiServiceProvider value={this.state.swapiService} >
           <div className="stardb-app">
-            <Header />
+            <Header onServiceChange={this.onServiceChange} />
 
             <PersonDetails itemId={11} />
 
