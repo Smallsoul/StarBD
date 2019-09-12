707ebd0d5716392227da8762dbd823fba99f6126
Smallsoul <36156971+Smallsoul@users.noreply.github.com>
2019-09-12 11:34:25 +0400

    make fake authorization

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,13 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages'
+import { 
+  PeoplePage, 
+  PlanetsPage, 
+  StarshipsPage, 
+  LoginPage, 
+  SecretPage 
+} from '../pages'
 
 import { SwapiServiceProvider } from '../swapi-service-context';
 
@@ -18,8 +24,15 @@ import { StarshipDetails } from '../sw-components';
 export default class App extends Component {
 
   state = {
-    swapiService: new SwapiService()
+    swapiService: new SwapiService(),
+    isLoggedIn: false
   };
+
+  onLogin = () => {
+    this.setState({
+      isLoggedIn: true
+    })
+  }
 
   onServiceChange = () => {
     this.setState(({ swapiService }) => {
@@ -33,6 +46,8 @@ export default class App extends Component {
   }
 
   render() {
+
+    const { isLoggedIn } = this.state
 
     return (
       <ErrorBoundry>
@@ -57,6 +72,10 @@ export default class App extends Component {
                         const { id } = match.params
                         return <StarshipDetails itemId={id} />
                       }} />
+              <Route path="/login" 
+                      render={() => <LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin} />} />
+              <Route path="/secret"
+                      render={() => <SecretPage isLoggedIn={isLoggedIn} />} />
 
             </div>
           </Router>
