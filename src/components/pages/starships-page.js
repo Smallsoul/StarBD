59bd5817dc2fb21a7f1dd2f0bd91223286ac5596
Smallsoul <36156971+Smallsoul@users.noreply.github.com>
2019-09-12 11:13:57 +0400

    adding optional parameters

--- src/components/pages/starships-page.js
+++ src/components/pages/starships-page.js
@@ -6,9 +6,7 @@ import { withRouter } from 'react-router-dom'
 
 const StarshipsPage = ({ history }) => {
         return (
-            <StarshipList onItemSelected={( itemId ) => {
-                history.push(`/starships/${itemId}`)
-            }} />
+            <StarshipList onItemSelected={( id ) => history.push(id)} />
         )
     }
 export default withRouter(StarshipsPage)
